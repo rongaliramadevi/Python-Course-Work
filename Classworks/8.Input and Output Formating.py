022e4fbb97037d79a6d17bb3b4f8f0d2f7f9f90f
Ms-Ramadevi <rongaliramadevi23@gmail.com>
2025-11-18 10:55:33 +0530

    8.Input ad Output Formating

---
+++ Classworks/8.Input and Output Formating.py
@@ -0,0 +1,43 @@
+#input formating
+
+s=input("Python") #string
+print(s)
+
+i=int(input()) #integer
+
+i=float(input()) #float
+
+l=list(map(int,input().split(','))) #list 
+
+name=input("Enter name:")
+print(name)
+age=int(inpur("Enter age:"))
+print(age)
+
+names=input()
+ruchitha, preethi, varsha
+print(names)
+
+names=list(map(int,input().split()))
+print(names)
+
+names=tuple(input().split())#tuple input for str
+print(names)
+
+names=set(map(int,input().split())) #set input()
+print(names)
+
+d=eval(input())
+{'name':'rama','age':21}
+print(d)
+
+a,b=list(map(int,input().split())) #multiple varibles assignment
+#output formating
+a=10
+b=20
+c=30
+print(a+b)
+print(a,b)
+print("a=",a,"b=",b)
+print(f'a={a}\nb={b}\nc={c}') #f strings
+print('a={}\nb={}\c={}'.format(a,b,c)) #formating
